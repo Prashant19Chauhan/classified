e68d2df8132b23a1937a278276652395b95bb69f
prash134angy <prashantchauhanwork@gmail.com>
2025-03-26 03:37:01 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -40,6 +40,8 @@ export const register = async (req, res) => {
 
 
 export const login = async(req, res) => {
+    console.log(req.body);
+    console.log("hii");
     try {
         const {idToken} = req.body;
 
