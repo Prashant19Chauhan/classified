20f01f557b40848c0f48c34c091e31e284cd85d3
prash134angy <prashantchauhanwork@gmail.com>
2025-03-27 01:29:02 +0530

    ...

--- classified-ads/src/api/authService.js
+++ classified-ads/src/api/authService.js
@@ -1,9 +1,6 @@
 import axios from "axios";
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8800/api/auth"; // Backend URL
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8800/api/auth";
 
 
 export const register = async (formData) => {
